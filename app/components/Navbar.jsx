5f541de5ba084f011e8e7573db3377a626f51884
shelavale3795 <pallavishelavale3795@gmail.com>
2025-04-14 07:09:34 +0530

    update code

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -7,13 +7,13 @@ const Navbar = () => {
   const {theme, toggleTheme} = useCarContext();
 
   return (
-    <div className={`shadow-sm h-32 sm:h-24 md:h-16  w-full flex flex-col sm:flex-row justify-center sm:justify-between items-center gap-2 px-8 ${theme === 'dark' ? 'bg-gray-900 text-white': ''}`}>
+    <div className={`shadow-sm h-32 sm:h-24 md:h-16  w-full flex flex-col sm:flex-row justify-center sm:justify-between items-center gap-2 px-8 border-b border-gray-200 ${theme === 'dark' ? 'bg-gray-900 text-white': ''}`}>
       <Link href="/" className="flex gap-2 items-center cursor-pointer">
         <img src="/car.png" alt="" className="w-10" />
         <p className="text-2xl font-semibold">CarFinder</p>
       </Link>
       <div className="flex gap-6 items-center">
-        <img src={theme === 'light' ? "/moon.svg" : "/sun.svg"} alt="" className="cursor-pointer" onClick={toggleTheme}/>
+        <img src={theme === 'light' ? "/moon.svg" : "/sun.svg"} alt="" className="cursor-pointer " onClick={toggleTheme}/>
         <div className="relative group">
           <Link href="/wishlist">
             <img src={theme === 'light' ? "/wishlist.svg" : "/wishlist-light.svg"} alt="" className="cursor-pointer" />
